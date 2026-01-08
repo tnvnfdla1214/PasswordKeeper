b3e7172aab68240bfa014b50fc7a5e848f3f92f3
mingyu <tnvnfdla12@gmail.com>
2026-01-08 14:48:31 +0900

    폰트 추가

--- app/src/main/java/com/passwordkeeper/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/passwordkeeper/presentation/ui/theme/Type.kt
@@ -7,11 +7,82 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 val Typography = Typography(
+    // T1 Medium 16pt
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp
+    ),
+    // T1 SemiBold 32pt
+    displayLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 32.sp
+    ),
+    // T1 Bold 20pt
+    displayMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+
+    //////////////////////////////////////
+
+    // T2 SemiBold 24pt
+    headlineLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 24.sp
+    ),
+    // T2 Bold 18pt
+    headlineMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
+    ),
+    // T2 Medium 15pt
+    headlineSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 15.sp
+    ),
+
+    ////////////////////////////////////////
+
+    // T3 SemiBold 20pt
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp
+    ),
+    // T4 SemiBold 16pt
+    titleMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp
+    ),
 )
+
+val Typography.t1Medium16: TextStyle
+    get() = bodyLarge
+
+val Typography.t1SemiBold32: TextStyle
+    get() = displayLarge
+
+val Typography.t1Bold20: TextStyle
+    get() = displayMedium
+
+val Typography.t2SemiBold24: TextStyle
+    get() = headlineLarge
+
+val Typography.t2Bold18: TextStyle
+    get() = headlineMedium
+
+val Typography.t2Medium15: TextStyle
+    get() = headlineSmall
+
+val Typography.t3SemiBold20: TextStyle
+    get() = titleLarge
+
+val Typography.t4SemiBold16: TextStyle
+    get() = titleMedium
