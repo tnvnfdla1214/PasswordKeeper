2fa9feab9b9a2d10d3a8fa1cd8e1852a0398b896
mingyu <tnvnfdla12@gmail.com>
2026-01-20 15:05:59 +0900

    패키지명 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.passwordkeeper"
+        applicationId = "com.mingyu.memorynote"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
