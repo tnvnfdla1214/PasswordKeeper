3bfbc70c1814736dfd7463ac2ccbcbaef1d6549e
mingyu <tnvnfdla12@gmail.com>
2026-01-21 16:17:39 +0900

    버전 코드 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.mingyu.memorynote"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
